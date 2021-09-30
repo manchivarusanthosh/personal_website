90c8edd6032a4ad0ef4ec1c836b065ef8bf4f76d
manchivarusanthosh <manchivarusanthoshkumar@gmail.com>
2021-09-30 21:40:52 +0530

    navbar and mobile view added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar/navbar';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-bootstrap/dist/react-bootstrap.min.js";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavBar() {
+
+    const [click,setClick] = useState(false)
+    return (
+        <>
+            <header id="header">
+                <div className="container">
+                    <h1>M Santhosh Kumar</h1>
+                    {/* <!-- Uncomment below if you prefer to use an image logo --> */}
+                    {/* <a href="index.html" className="mr-auto"><img src="assets/img/logo.png" alt="" className="img-fluid"/></a> */}
+                    <h2>I'm a passionate <span>Full Stack Developer</span> from India</h2>
+
+                    <nav id="navbar" className={`${click ?"navbar navbar-mobile":"navbar"}`}>
+                        <ul>
+                            <li><a className="nav-link active" href="#header">Home</a></li>
+                            <li><a className="nav-link" href="#about">About</a></li>
+                            <li><a className="nav-link" href="#resume">Resume</a></li>
+                            <li><a className="nav-link" href="#contact">Contact</a></li>
+                        </ul>
+                        <i 
+                        className={`${click ? "fas fa-times mobile-nav-toggle" : "fas fa-bars mobile-nav-toggle" }`}
+                        onClick={()=>setClick(!click)}
+                        ></i>
+                    </nav>
+
+                    <div className="social-links">
+                        <a href="https://twitter.com/" className="twitter"><i className="fab fa-twitter"></i></a>
+                        <a href="https://m.facebook.com/" className="facebook"><i className="fab fa-facebook"></i></a>
+                        <a href="https://instagram.com/" className="instagram"><i className="fab fa-instagram"></i></a>
+                        <a href="https://linkedin.com/" className="linkedin"><i className="fab fa-linkedin"></i></a>
+                    </div>
+
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default NavBar
